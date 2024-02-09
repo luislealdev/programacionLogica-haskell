ae9a454c63e204a48e5c4e5555e6272d21cbfcb5
Luis Leal <luisrrleal@gmail.com>
2024-02-09 08:16:15 -0600

    Add some functions

---
+++ media.hs
@@ -0,0 +1,12 @@
+media3 x y z = (x+y+z)/3
+
+volumenEsfera radio = (4/3*3.1416*radio^3)
+
+residuo x = rem x 2
+
+rota n xs = drop n xs ++ take n xs 
+
+palindro xs = xs == reverse xs
+
+interior xs = tail (init xs)
+
