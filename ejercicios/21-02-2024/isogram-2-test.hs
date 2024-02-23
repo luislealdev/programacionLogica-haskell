4e78f67fbc6f56f8bdf68de448f99899dc9de0eb
Luis Leal <luisrrleal@gmail.com>
2024-02-23 08:18:16 -0600

    Fix error with isogram-2

--- ejercicios/21-02-2024/isogram-2-test.hs
+++ ejercicios/21-02-2024/isogram-2-test.hs
@@ -1,6 +1,7 @@
 module Isogram2 (isIsogram2) where
 
 import Data.List (nub)
+import Data.Char (toLower)
 
 isIsogram2 :: String -> Bool
-isIsogram2 str = length (nub str) == length str
+isIsogram2 str = length (nub (map toLower str)) == length str
